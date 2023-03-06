82705a558808c774c5d2488a67889fbeb131cb2a
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-03-06 08:33:00 +0200

    Update display-quiz.js
    "bug due to working in live server"

--- portfolio projects/Apps/Quiz application/js/display-quiz.js
+++ portfolio projects/Apps/Quiz application/js/display-quiz.js
@@ -212,7 +212,7 @@ const  data = async()=>{
 
 //-[10]
     const confirmAtteptance = () => {
-        if (sessionStorage.length !== questionsArr.length + 1) {
+        if (sessionStorage.length !== questionsArr.length) {
             window.alert('there are still questions that you haven\'t seen yet')
 
         }
