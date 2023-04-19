2ef8a9f49c311a208c8c5517bbd69028557551ae
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-04-19 06:04:25 +0200

    add react certificate
    'add react certificate'

---
+++ portfolio projects/React Applications/calculator/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import CalculatorApp from './CalculatorApp';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <CalculatorApp />
+);
+
+reportWebVitals();
